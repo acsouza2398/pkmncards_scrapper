7dae0f469d52e7b6c13fb21517448c49aadf37e7
acsouza2398 <anaclgs@al.insper.edu.br>
2024-08-16 11:02:46 -0300

    chore: Add docustrings to classes

--- src/compile.py
+++ src/compile.py
@@ -4,19 +4,39 @@ import os
 import json
 
 class Compile():
+    """Compile the cards
+    1. Add the data
+    2. Save the data to a parquet file
+    """
     def __init__(self) -> None:
         self.data: list = []
 
-    def run(self) -> Any:
+    def run(self) -> None:
+        """
+        Run the compiler
+        1. Add the data
+        2. Save the data to a parquet file
+        """        
         self.__add_data()
         df:any = pd.concat(objs=self.data, axis=0)
         self.__save_to_parquet(df=df)
 
     def __add_data(self) -> None:
+        """
+        Add the data
+        1. Loop through the files
+        2. Open the file
+        3. Load the data
+        4. Append the data
+        """
         for file in os.listdir(path="output/filtered"):
             with open(file=f"output/filtered/{file}", mode="r", encoding="utf-8") as f:
                 data: Any = json.load(fp=f)
                 self.data.append(pd.DataFrame(data=data))
     
     def __save_to_parquet(self, df: Any) -> None:
+        """
+        Save the data to a parquet file
+        1. Save the data to a parquet file
+        """
         df.to_parquet("output/compiled_cards.parquet", engine="pyarrow")
--- src/filter.py
+++ src/filter.py
@@ -6,6 +6,12 @@ import json
 key: dict[int, str] = {0: "name", 1: "text"}
 
 class Filter():
+    """Filter the cards
+    1. Set the patterns
+    2. Filter the cards
+    3. Find the information
+    4. Save the filtered cards
+    """    
     def __init__(self, file_path) -> None:
         self.file_path: Any = file_path
         self.filtered_cards: dict = []
@@ -13,12 +19,20 @@ class Filter():
 
     def run(self) -> None:
         """Run the filter
-        1. Filter the cards
+        1. Set the patterns
+        2. Filter the cards
         """
         self.__set_patterns()
         self.__filter_cards()
 
     def __set_patterns(self) -> None:
+        """
+        Set the patterns
+        1. Set the name pattern
+        2. Set the text pattern
+        3. Set the card type
+        4. Set the pattern
+        """        
         name = r"(?:^)(.*?)(?= ›)"
         text = r"(?:› )(.*?)(?=$)"
 
@@ -36,7 +50,19 @@ class Filter():
         self.pattern: list[Pattern[str]] = [name, text]
 
     def __find_information(self, card: str) -> dict:
-        
+        """
+        Find the information
+        1. Loop through the patterns
+        2. Find the match
+        3. Set the card information
+        4. Return the card information
+
+        Args:
+            card (str): The card to find the information
+
+        Returns:
+            dict: The card information extracted
+        """        
         card_information: dict = {}
         for index, pattern in enumerate(iterable=self.pattern):
             match: re.Match[str] | None = re.findall(pattern=pattern, string=card)
@@ -47,6 +73,12 @@ class Filter():
         return card_information
     
     def __filter_cards(self) -> None:
+        """Filter the cards
+        1. Open the file
+        2. Loop through the cards
+        3. Find the information
+        4. Save the filtered cards
+        """
         with open(file=self.file_path, mode="r", encoding="utf-8") as file:
             cards: list[str] = file.readlines()
             for card in cards:
@@ -57,5 +89,8 @@ class Filter():
         self.__save_filtered_cards()
 
     def __save_filtered_cards(self) -> None:
+        """Save the filtered cards
+        1. Save the filtered cards to a file
+        """
         with open(file=f"output/filtered/filtered_cards_{self.card_type}.json", mode="w", encoding="utf-8") as file:
             json.dump(obj=self.filtered_cards, fp=file, indent=4, ensure_ascii=False)
